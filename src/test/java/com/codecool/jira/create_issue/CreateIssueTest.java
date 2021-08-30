2e62a067f1d00f3760b1bf0bd55bd168fd824ef9
popesz2000 <popesz2000@gmail.com>
2021-08-30 21:07:02 +0200

    Create issues.csv

--- src/test/java/com/codecool/jira/create_issue/CreateIssueTest.java
+++ src/test/java/com/codecool/jira/create_issue/CreateIssueTest.java
@@ -1,4 +1,28 @@
 package com.codecool.jira.create_issue;
 
-public class CreateIssueTest {
+import com.codecool.jira.MainTest;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import page_object_model.login.LogIn;
+
+public class CreateIssueTest extends MainTest {
+    LogIn login;
+
+    @Before
+    public void setup() {
+        super.setUp();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/issues.csv", numLinesToSkip = 1)
+    public void createIssue() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        super.tearDown();
+    }
 }
