de69780d2e1eb6fa3290fb17cce60453720f619a
popesz2000 <popesz2000@gmail.com>
2021-08-31 09:56:06 +0200

    Make test fail when user dont have access

--- src/test/java/com/codecool/jira/create_issue/CreateIssueTest.java
+++ src/test/java/com/codecool/jira/create_issue/CreateIssueTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import page_object_model.Dashboard;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreateIssueTest extends MainTest {
     Dashboard dashboard;
@@ -19,14 +19,14 @@ public class CreateIssueTest extends MainTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/create_issue.csv", numLinesToSkip = 1)
-    public void createIssue(String user, String projectName, boolean hasAccess) {
+    public void createIssue(String user, String projectName) {
         dashboard = new Dashboard(driver);
 
         dashboard.openURL("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa");
         dashboard.login(dotenv.get(user), dotenv.get("PASSWORD"));
 
         dashboard.createIssueOnlyProject(projectName);
-        assertEquals(hasAccess, dashboard.validateProjectPicture());
+        assertTrue(dashboard.validateProjectPicture());
     }
 
     @AfterEach
