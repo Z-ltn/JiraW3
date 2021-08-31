1614d5710dd197100a04d308baca164c3a412917
popesz2000 <popesz2000@gmail.com>
2021-08-31 12:45:12 +0200

    feat: components with glass package and class

---
+++ src/test/java/com/codecool/jira/components_with_glass/ComponentsWithGlassTest.java
@@ -0,0 +1,20 @@
+package com.codecool.jira.components_with_glass;
+
+import com.codecool.jira.MainTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+
+public class ComponentsWithGlassTest extends MainTest {
+    @Before
+    public void setup() {
+        super.setUp();
+    }
+
+
+
+    @AfterEach
+    public void tearDown() {
+        super.tearDown();
+    }
+}
