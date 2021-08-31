6adab1ee12e42938caa5bae8da5f7a1e7ec94fe3
balintovics <molnar99b@gmail.com>
2021-08-31 14:33:35 +0200

    feat: browse projects test cases

--- src/test/java/com/codecool/jira/MainTest.java
+++ src/test/java/com/codecool/jira/MainTest.java
@@ -7,13 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.Util;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class MainTest {
     protected static WebDriver driver;
     protected static WebDriverWait wait;
     protected static Dotenv dotenv;
+    //protected static Set<String> users = new HashSet<>(Arrays.asList("USER1", "USER2", "USER3", "USER4"));
 
     @BeforeEach
     protected void setUp() {
@@ -22,7 +26,6 @@ public class MainTest {
         }
         driver = Util.getDriver();
         dotenv = Dotenv.load();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         wait = Util.wait(driver, 10);
     }
