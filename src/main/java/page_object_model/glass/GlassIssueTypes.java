d36b71754346f30b37db64461d512e5f8e879981
popesz2000 <popesz2000@gmail.com>
2021-09-02 10:08:42 +0200

    feat: issueTypesInGlass test

--- src/main/java/page_object_model/glass/GlassIssueTypes.java
+++ src/main/java/page_object_model/glass/GlassIssueTypes.java
@@ -1,4 +1,24 @@
 package page_object_model.glass;
 
-public class GlassIssueTypes {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import page_object_model.PageBase;
+
+import java.util.List;
+
+public class GlassIssueTypes extends PageBase {
+    JavascriptExecutor js;
+
+    public GlassIssueTypes(WebDriver driver) {
+        super(driver);
+        js = (JavascriptExecutor) driver;
+    }
+
+    public List<String> getTypes() {
+        return (List<String>) js.executeScript("let c = document.querySelector('#project-config-webpanel-summary-issuetypes > div.mod-content > div > ul'); let expected = []; for (let i=0;i<c.children.length;i++) {expected.push(c.children[i].firstElementChild.lastElementChild.textContent.trim().toLowerCase());} return expected;");
+    }
+
+    public List<String> getActualTypes() {
+        return (List<String>) js.executeScript("var actual = []; var td = document.querySelector('#glass-general-panel > div.aui-group > div:nth-child(1) > div > table > tbody > tr:nth-child(8) > td.glass-meta-value'); for (let i=0;i<td.children.length;i++) {actual.push(td.children[i].getAttribute('title').split(' ')[1].toLowerCase());} actual.sort(); var res = []; actual.forEach(ele => res.push(ele.substring(0, ele.length-1))); return res;");
+    }
 }
--- src/test/java/com/codecool/jira/issue_types_in_glass/IssueTypesInGlassTest.java
+++ src/test/java/com/codecool/jira/issue_types_in_glass/IssueTypesInGlassTest.java
@@ -1,4 +1,24 @@
 package com.codecool.jira.issue_types_in_glass;
 
-public class IssueTypesInGlassTest {
+import com.codecool.jira.MainTest;
+import org.junit.jupiter.api.Test;
+import page_object_model.glass.GlassIssueTypes;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IssueTypesInGlassTest extends MainTest {
+    GlassIssueTypes glassIssueTypes;
+
+    @Test
+    public void issueTypesInGlass() {
+        glassIssueTypes = new GlassIssueTypes(driver);
+        glassIssueTypes.login(dotenv.get("USER4"), dotenv.get("PASSWORD"));
+        glassIssueTypes.openURL("https://jira-auto.codecool.metastage.net/plugins/servlet/project-config/PP/summary");
+        List<String> types = glassIssueTypes.getTypes();
+        glassIssueTypes.openURL("https://jira-auto.codecool.metastage.net/projects/PP?selectedItem=com.codecanvas.glass:glass");
+        List<String> actual = glassIssueTypes.getActualTypes();
+        assertEquals(types, actual);
+    }
 }
